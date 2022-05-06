9d4f9cd023a3ba81e1b0b5820069b9d164d783f7
EMT119 <austinarbaugh16@gmail.com>
2022-05-06 17:56:38 -0400

    initial upload

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  <div className="navbar">
+    <BrowserRouter>
+      <NavLink to="/">Dashboard</NavLink>
+      <NavLink to></NavLink>
+    </BrowserRouter>
+  </div>;
+};
+
+export default Navbar;
