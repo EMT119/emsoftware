5dcee5fa11632f179a92d078a11227566ecb4230
EMT119 <austinarbaugh16@gmail.com>
2022-05-06 18:03:32 -0400

    Initial

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { BrowserRouter, NavLink } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
-  <div className="navbar">
-    <BrowserRouter>
-      <NavLink to="/">Dashboard</NavLink>
-      <NavLink to></NavLink>
-    </BrowserRouter>
-  </div>;
+  return (
+    <div className="navbar">
+      <BrowserRouter>
+        <NavLink to="/">Dashboard</NavLink>
+        <NavLink to="schedule">Schedule</NavLink>
+        <NavLink to="members">Members</NavLink>
+        <NavLink to="Forms">Forms</NavLink>
+        <NavLink className="flex-right" to="Logout">
+          Logout
+        </NavLink>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default Navbar;
